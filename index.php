11c4b0c7f56164c294c7ef40dd77786cb58d20bf
SkyDreamer223 <56580969+SkyDreamer223@users.noreply.github.com>
2021-01-28 11:54:21 +0100

    final

--- index.php
+++ index.php
@@ -43,13 +43,14 @@
             Résérvations
         </h2>
         <ul>
-            <li>Paris</li>
-            <li>Phone: +33 07 11 24 33 00</li>
-            <li>Email: mail@gmail.com</li>
-            <li><a href="#">Retour à l'accueil</a></li>
+            <li><i class="fas fa-map-marker"></i> Paris</li>
+            <li><i class="fas fa-phone"></i> Phone: +33 07 11 24 33 00</li>
+            <li><i class="fas fa-envelope"></i> Email: mail@gmail.com</li>
+            <li><a href="#"><i class="fas fa-sort-up"></i> Retour à l'accueil</a></li>
         </ul>
         <p>&copy; Design by MIT <?php print date('Y'); ?></p>
     </footer>
     <script src="./js/app.js"></script>
+    <script src="https://kit.fontawesome.com/1b981478ef.js" crossorigin="anonymous"></script>
 </body>
 </html>
